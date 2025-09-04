1962dc867b5c4f4cf28b7207cc17cad6853cfea0
shiraj pradhanang <shirajpng@gmail.com>
2025-09-04 12:42:42 +0700

    OOP implemented with addition of models.py

--- calculator-server/main.py
+++ calculator-server/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from asteval import Interpreter
 
 from calculator import expand_percent
+from models import Expression, CalculatorLog
 
 HISTORY_MAX = 1000
 history = deque(maxlen=HISTORY_MAX)
@@ -24,20 +25,20 @@ aeval = Interpreter(minimal=True, usersyms={"pi": math.pi, "e": math.e})
 
 
 @app.post("/calculate")
-def calculate(expr: str):
+def calculate(expr_obj: Expression):
     try:
-        code = expand_percent(expr)
-        result = aeval(code)
+        # code = expand_percent(expr)
+        result = aeval(expr_obj.expand_percent())
         if aeval.error:
             msg = "; ".join(str(e.get_error()) for e in aeval.error)
             aeval.error.clear()
-            return {"ok": False, "expr": expr, "result": "", "error": msg}
-        history.append({"timestamp": datetime.now(), "expr": expr, "result": result})
-        return {"ok": True, "expr": expr, "result": result, "error": ""}
+            return {"ok": False, "expr": expr_obj.expr, "result": "", "error": msg}
+        history.append({"timestamp": datetime.now(), "expr": expr_obj.expr, "result": result})
+        return {"ok": True, "expr": expr_obj.expr, "result": result, "error": ""}
     except Exception as e:
-        return {"ok": False, "expr": expr, "error": str(e)}
+        return {"ok": False, "expr": expr_obj.expr, "error": str(e)}
 
-@app.get("/history")
+@app.get("/history", response_model=list[CalculatorLog])
 def get_history(limit: int | None = None):
     return list(reversed(history))[:limit] if limit else list(reversed(history))
 
